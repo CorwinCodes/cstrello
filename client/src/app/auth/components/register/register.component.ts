aac57f229403cb23d64ac3bae01985a99e5ef759
CorwinCodes <corwinscott@protonmail.com>
2023-03-30 18:14:23 -0500

    register and login functionality for client interface and back-end api calls integrated

--- client/src/app/auth/components/register/register.component.ts
+++ client/src/app/auth/components/register/register.component.ts
@@ -1,20 +1,38 @@
+import { HttpErrorResponse } from "@angular/common/http";
 import { Component } from "@angular/core";
 import { FormBuilder, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { AuthService } from "../../services/auth.service";
+import { RegisterRequestInterface } from "../../types/registerRequest.interface";
 
 @Component({
     selector: 'auth-register',
     templateUrl: './register.component.html',
 })
 export class RegisterComponent {
+    errorMessage: string | null = null;
     form = this.fb.group({
         email: ['', Validators.required],
         username: ['', Validators.required],
         password: ['', Validators.required],
-    })
+    });
 
-    constructor(private fb: FormBuilder){}
+    constructor(private fb: FormBuilder, private authService: AuthService, private router:Router){} //inject auth service to register
 
-    onSubmit(){
-        console.log('onSubmit', this.form.value)
+    onSubmit():void { //note that register returns an observable so we subscribe
+        
+        this.authService.register(this.form.value as RegisterRequestInterface).subscribe({ //note this typing was necessary because form values include null by default
+            next: (currentUser) => {
+                console.log('currentUser', currentUser) //see this in the console so we can use it to store token and set user for client
+                this.authService.setToken(currentUser);
+                this.authService.setCurrentUser(currentUser);
+                this.errorMessage = null;
+                this.router.navigate(['/'])
+            },
+            error: (err: HttpErrorResponse) => { //we know the error type, so we type it here ||| we can use this error on template for error messages
+                console.log('err', err.error); //since we typed it we know we can add the .error to err
+                this.errorMessage = err.error.join(', '); //we know from looking at controllers on server that erros are string[], so join works
+            },
+        })
     }
 }
--- client/src/app/auth/services/auth.service.ts
+++ client/src/app/auth/services/auth.service.ts
@@ -3,11 +3,28 @@ import { BehaviorSubject, Observable } from "rxjs";
 import { HttpClient } from "@angular/common/http"
 import { CurrentUserInterface } from "../types/currentUser.interface";
 import { environment } from "src/environments/environment";
+import { RegisterRequestInterface } from "../types/registerRequest.interface";
+import { LoginRequestInterface } from "../types/loginRequestInterface";
 
 @Injectable()
 export class AuthService {
     currentUser$ = new BehaviorSubject<CurrentUserInterface | null | undefined>(undefined);
     constructor(private http: HttpClient) {}
+    
+    register(registerRequest: RegisterRequestInterface): Observable<CurrentUserInterface> { //note proper typing of var and output
+        const url = environment.apiUrl + '/users';
+        return this.http.post<CurrentUserInterface>(url, registerRequest);//still need to store token w/ set token method, not typingon post
+    }
+
+    login(loginRequest: LoginRequestInterface): Observable<CurrentUserInterface> {
+        const url = environment.apiUrl + '/users/login';
+        return this.http.post<CurrentUserInterface>(url, loginRequest);
+    }
+
+    setToken(currentUser: CurrentUserInterface):void { //now this exists use it in submit on register component
+        localStorage.setItem('token', currentUser.token)
+    }
+
     getCurrentUser():Observable<CurrentUserInterface> {
         const url = environment.apiUrl + '/user';
         return this.http.get<CurrentUserInterface>(url);
