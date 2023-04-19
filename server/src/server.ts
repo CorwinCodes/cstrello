303304c9d34e7299ee8f53024a90ea883c2e9f3c
CorwinCodes <45740723+CorwinCodes@users.noreply.github.com>
2023-04-19 16:39:23 -0500

    minor changes for heroku

--- server/src/server.ts
+++ server/src/server.ts
@@ -33,7 +33,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
 if(envs.SERVE_FRONTEND === 'true'){
-    app.use('/', express.static('../../client/dist/client'));
+    app.use('/', express.static('./../../../client/dist/client'));
 }
 
 app.post('/api/users', usersController.register);
